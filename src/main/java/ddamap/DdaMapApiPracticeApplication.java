de938bad920037b579f6ae5163be6ac39db3b73a
kiteof_park <kiteof_park@naver.com>
2025-07-17 20:46:52 +0900

    initial commit

---
+++ src/main/java/ddamap/DdaMapApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package ddamap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdaMapApiPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdaMapApiPracticeApplication.class, args);
+    }
+
+}
