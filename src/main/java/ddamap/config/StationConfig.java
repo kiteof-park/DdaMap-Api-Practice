e1b48cff1de5b617747bc41aa7099eeed7e37ab4
kiteof_park <kiteof_park@naver.com>
2025-07-18 18:45:11 +0900

    feat :  web client config  설정(응답 버퍼 사이즈 설정)

---
+++ src/main/java/ddamap/config/StationConfig.java
@@ -0,0 +1,23 @@
+package ddamap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * WebClient 빈 생성 및 설정
+ */
+@Configuration
+public class StationConfig {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder()
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer
+                                .defaultCodecs()
+                                .maxInMemorySize(10 * 1024 * 1024)) // 10MB
+                        .build());
+    }
+}
