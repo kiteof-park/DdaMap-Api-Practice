ee502bac06ed8e39e352d10271683c5d309ae528
kiteof_park <kiteof_park@naver.com>
2025-07-22 20:35:02 +0900

    feat : dto 구현

---
+++ src/main/java/ddamap/dto/RealtimeStationFullResponse.java
@@ -0,0 +1,15 @@
+package ddamap.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record RealtimeStationFullResponse(
+        @JsonProperty("rentBikeStatus")
+        RentBikeStatus rentBikeStatus
+) {
+    public record RentBikeStatus(
+            @JsonProperty("row")
+            List<RealtimeStationResponse> row
+    ){}
+}
---
+++ src/main/java/ddamap/dto/RealtimeStationResponse.java
@@ -0,0 +1,15 @@
+package ddamap.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record RealtimeStationResponse(
+        @JsonProperty("rackTotCnt")  String rackTotCnt,                 // 거치대 개수
+        @JsonProperty("parkingBikeTotCnt") String parkingBikeTotCnt,    // 자전거 주차 총 건수
+        @JsonProperty("shared") String shared,                          // 거치율
+        @JsonProperty("stationLatitude") double stationLatitude,        // 위도
+        @JsonProperty("stationLongitude") double stationLongitude,      // 경도
+        @JsonProperty("stationId") String stationId,                    // 대여소ID
+        @JsonProperty("stationName") String stationName                 // 대여소 이름
+) {
+    // TODO: toEntity() 필요?
+}
