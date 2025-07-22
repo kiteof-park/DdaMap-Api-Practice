a1662ca7d23c609794d2e8bc030d651534933463
kiteof_park <kiteof_park@naver.com>
2025-07-22 20:35:58 +0900

    feat : test controller 작성

--- src/main/java/ddamap/controller/StationController.java
+++ src/main/java/ddamap/controller/StationController.java
@@ -1,19 +1,31 @@
 package ddamap.controller;
 
+import ddamap.dto.RealtimeStationResponse;
+import ddamap.service.RealtimeStatitonFetchServiceImpl;
 import ddamap.service.StationFetchServiceImpl;
 import ddamap.service.StationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/stations")
 public class StationController {
 
-    private final StationFetchServiceImpl stationFetchService;
+    private final StationFetchServiceImpl stationService;
+    private final RealtimeStatitonFetchServiceImpl realtimeStatitonService;
 
-    @GetMapping("/api/station/sync")
+    @GetMapping("/sync")
     public void syncStations() {
-        stationFetchService.syncStations();
+        stationService.syncStations();
+    }
+
+    @GetMapping("/realtime")
+    public List<RealtimeStationResponse> getRealtimeStations() {
+        return realtimeStatitonService.getAllRealtimeStations();
     }
 }
