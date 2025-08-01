e87feabee7c2b35118780df5416a331830576eee
kiteof_park <kiteof_park@naver.com>
2025-08-01 21:51:41 +0900

    feat: 키워드 검색 컨트롤러 구현

--- src/main/java/ddamap/controller/KakaoLocalController.java
+++ src/main/java/ddamap/controller/KakaoLocalController.java
@@ -1,22 +1,31 @@
 package ddamap.controller;
 
 import ddamap.client.KakaoLocalClient;
-import ddamap.dto.Coordinate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/coordinate")
 public class KakaoLocalController {
 
     private final KakaoLocalClient client;
 
-    @GetMapping("/coordinate")
-    public ResponseEntity<?> getCoordinate(@RequestParam String address) {
+    @GetMapping("/address")
+    public ResponseEntity<?> getCoordinateByAddress(@RequestParam String address) {
         return client.getCoordinateByAddress(address)
+                .map(coordinate -> ResponseEntity.ok().body(coordinate))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // TODO : getCoordinateByKeyword() 구현 및 주소/키워드 검색 판별 전략 구현
+    @GetMapping("/keyword")
+    public ResponseEntity<?> getCoordinateByKeyword(@RequestParam String keyword) {
+        return client.getCoordinateByKeyword(keyword)
                 .map(coordinate -> ResponseEntity.ok().body(coordinate))
                 .orElse(ResponseEntity.notFound().build());
     }
