a71f041fb641219fb9a90089b706f69efab5b10c
kiteof_park <kiteof_park@naver.com>
2025-08-14 18:30:24 +0900

    style: 비활성 필드 주석처리

--- src/main/java/ddamap/dto/RealtimeStationResponse.java
+++ src/main/java/ddamap/dto/RealtimeStationResponse.java
@@ -3,9 +3,9 @@ package ddamap.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record RealtimeStationResponse(
-        @JsonProperty("rackTotCnt")  String rackTotCnt,                 // 거치대 개수
+//        @JsonProperty("rackTotCnt")  String rackTotCnt,                 // 거치대 개수
+//        @JsonProperty("shared") String shared,                          // 거치율
         @JsonProperty("parkingBikeTotCnt") String parkingBikeTotCnt,    // 자전거 주차 총 건수
-        @JsonProperty("shared") String shared,                          // 거치율
         @JsonProperty("stationLatitude") double stationLatitude,        // 위도
         @JsonProperty("stationLongitude") double stationLongitude,      // 경도
         @JsonProperty("stationId") String stationId,                    // 대여소ID
