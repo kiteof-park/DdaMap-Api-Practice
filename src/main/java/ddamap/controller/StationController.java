e72294d77c67d626b8b93254959597b271116e81
kiteof_park <kiteof_park@naver.com>
2025-08-14 18:31:44 +0900

    feat: 인근 대여소 정보 반환 컨트롤러 구현

--- src/main/java/ddamap/controller/StationController.java
+++ src/main/java/ddamap/controller/StationController.java
@@ -1,11 +1,13 @@
 package ddamap.controller;
 
 import ddamap.dto.RealtimeStationResponse;
+import ddamap.dto.StationResponse;
 import ddamap.service.RealtimeStatitonFetchServiceImpl;
 import ddamap.service.StationFetchServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,13 +20,39 @@ public class StationController {
     private final StationFetchServiceImpl stationService;
     private final RealtimeStatitonFetchServiceImpl realtimeStatitonService;
 
+    /**
+     * 따릉이 대여소 정적 정보 호출 -> 스케줄링
+     */
     @GetMapping("/sync")
     public void syncStations() {
         stationService.syncStations();
     }
 
+    /**
+     * 따릉이 전체 대여소 실시간 정보 호출
+     * @return
+     */
     @GetMapping("/realtime")
     public List<RealtimeStationResponse> getRealtimeStations() {
         return realtimeStatitonService.getAllRealtimeStations();
+    }
+
+    /**
+     * 현재 위치 반경 따릉이 대여소 실시간 정보 호출
+     * @param latitude 위도
+     * @param longitude 경도
+     * @param radius 반경
+     * @param limit 인근 대여소 개수 제한
+     * @param concurrency 동시 요청 호출 수 제한
+     * @return 현재 위치 반경 1km 이내 따릉이 대여소 실시간 정보
+     */
+    @GetMapping("/nearby")
+    public List<RealtimeStationResponse> getNearbyStatitons(
+                                            @RequestParam double latitude,
+                                            @RequestParam double longitude,
+                                            @RequestParam(defaultValue = "1000") int radius,
+                                            @RequestParam(defaultValue = "15") int limit,
+                                            @RequestParam(defaultValue = "5") int concurrency){
+        return realtimeStatitonService.getNearbyStations(latitude, longitude, radius, limit, concurrency);
     }
 }
