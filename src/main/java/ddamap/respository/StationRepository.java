82cf4a623ad5fb974566f20fccb3b2835f87239b
kiteof_park <kiteof_park@naver.com>
2025-08-14 18:31:53 +0900

    feat: 인근 대여소 정보 반환 레포지토리 구현

--- src/main/java/ddamap/respository/StationRepository.java
+++ src/main/java/ddamap/respository/StationRepository.java
@@ -2,8 +2,38 @@ package ddamap.respository;
 
 import ddamap.domain.Station;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StationRepository extends JpaRepository<Station, Long> {
+
+    @Query(value = """
+        SELECT 
+            s.station_id       AS stationId,
+            s.name             AS stationName,
+            s.latitude         AS latitude,
+            s.longitude        AS longitude
+        FROM stations s
+        WHERE ST_DWithin(
+                s.geography,
+                ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)::geography,
+                :radiusMeters
+              )
+        ORDER BY ST_Distance(
+                 s.geography
+                 ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326)::geography
+              )
+        LIMIT :limit
+        """, nativeQuery = true)
+    List<NearbyStationBase> findNearbyStationBase(
+            @Param("longitude") double longitude,
+            @Param("latitude") double latitude,
+            @Param("radius") int radius,
+            @Param("limit") int limit
+    );
+
 }
