356a5d2adfa0934434d7a15384b66cc047eb9590
kiteof_park <kiteof_park@naver.com>
2025-08-16 03:50:19 +0900

    feat: nearby 서비스 구현

--- src/main/java/ddamap/service/RealtimeStatitonFetchServiceImpl.java
+++ src/main/java/ddamap/service/RealtimeStatitonFetchServiceImpl.java
@@ -1,11 +1,15 @@
 package ddamap.service;
 
 import ddamap.client.RealtimeStationApiClient;
+import ddamap.dto.NearbyStationResponse;
 import ddamap.dto.RealtimeStationResponse;
+import ddamap.respository.NearbyStationBase;
+import ddamap.respository.StationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,9 +18,40 @@ import java.util.List;
 public class RealtimeStatitonFetchServiceImpl {
 
     private final RealtimeStationApiClient apiClient;
+    private final StationRepository stationRepository;
 
-    // 서비스 레이어 스켈레톤
+    /**
+     * @return 모든 따릉이 대여소 실시간 정보
+     */
     public List<RealtimeStationResponse> getAllRealtimeStations() {
         return apiClient.getAllRealtimeStations();
+    }
+
+    /**
+     *
+     * @param latitude
+     * @param longitude
+     * @param radius
+     * @param limit
+     * @param concurrency
+     * @return
+     */public List<NearbyStationResponse> getNearbyStations(double latitude,
+                                                            double longitude,
+                                                            int radius,
+                                                            int limit,
+                                                            int concurrency){
+        // 현재 좌표 반경 내 대여소 리스트
+        final List<NearbyStationBase> bases = stationRepository.findNearbyStationBase(longitude, latitude, radius, limit);
+        if(bases.isEmpty()){
+            return List.of();
+        }
+
+        // 대여소별로 실시간 api 개별 호출
+        List<NearbyStationResponse> responses = new ArrayList<>();
+        for(NearbyStationBase base : bases){
+            apiClient.getRealtimeStation(base.getStationId())
+                    .ifPresent(responses::add);
+        }
+        return responses;
     }
 }
