e0189496b50279134c36b984501a49f4b8a18ef5
kiteof_park <kiteof_park@naver.com>
2025-08-16 07:06:35 +0900

    docs: swagger 추가

--- src/main/java/ddamap/controller/StationController.java
+++ src/main/java/ddamap/controller/StationController.java
@@ -4,6 +4,9 @@ import ddamap.dto.NearbyStationResponse;
 import ddamap.dto.RealtimeStationResponse;
 import ddamap.service.RealtimeStationFetchServiceImpl;
 import ddamap.service.StationFetchServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,43 +19,41 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/stations")
+@Tag(name = "따릉이 대여소 API", description = "따릉이 대여소 정적/실시간 정보 조회 API")
 public class StationController {
 
     private final StationFetchServiceImpl stationService;
     private final RealtimeStationFetchServiceImpl realtimeStationService;
 
-    /**
-     * 따릉이 대여소 정적 정보 호출 -> 스케줄링
-     */
+    @Operation(
+            summary = "따릉이 대여소 정적 정보 동기화",
+            description = "하루 1회 따릉이 전체 대여소 정보를 가져와 동기화")
     @GetMapping("/sync")
     public void syncStations() {
         stationService.syncStations();
     }
 
-    /**
-     * @return 따릉이 전체 대여소 실시간 정보 호출
-     */
+
+    @Operation(
+            summary = "실시간 따릉이 대여소 정보 조회",
+            description = "전체 대여소의 실시간 상태를 조회"
+    )
     @GetMapping("/realtime")
     public List<RealtimeStationResponse> getRealtimeStations() {
         return realtimeStationService.getAllRealtimeStations();
     }
 
-    /**
-     * 현재 위치 반경 따릉이 대여소 실시간 정보 호출
-     * @param latitude 위도
-     * @param longitude 경도
-     * @param radius 반경
-     * @param limit 인근 대여소 개수 제한
-     * @param concurrency 동시 요청 호출 수 제한
-     * @return 현재 위치 반경 1km 이내 따릉이 대여소 실시간 정보
-     */
+    @Operation(
+            summary = "현재 위치 1km반경 인근 대여소 실시간 정보 조회",
+            description = "위도/경도를 기준으로 반경 내의 실시간 따릉이 대여서 정보를 반환"
+    )
     @GetMapping("/nearby")
     public ResponseEntity<List<NearbyStationResponse>> getNearbyStations(
-                                            @RequestParam double latitude,
-                                            @RequestParam double longitude,
-                                            @RequestParam(defaultValue = "1000") int radius,
-                                            @RequestParam(defaultValue = "15") int limit,
-                                            @RequestParam(defaultValue = "5") int concurrency){
+            @Parameter(description = "위도") @RequestParam double latitude,
+            @Parameter(description = "경도") @RequestParam double longitude,
+            @Parameter(description = "검색 반경") @RequestParam(defaultValue = "1000") int radius,
+            @Parameter(description = "최대 대여소 개수") @RequestParam(defaultValue = "15") int limit,
+            @Parameter(description = "동시 요청 수 제한") @RequestParam(defaultValue = "5") int concurrency){
         List<NearbyStationResponse> stations = realtimeStationService.getNearbyStations(latitude, longitude, radius, limit, concurrency);
         return ResponseEntity.ok(stations);
     }
